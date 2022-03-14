5fe09ea51f34a18a507641b5654e547f95b8ea11
hironomiu <hironomiu@gmail.com>
2022-03-14 16:56:29 +0900

    commited

--- pages/hasura-fetch.tsx
+++ pages/hasura-fetch.tsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
 import Layout from '../components/Layout'
-import { useQuery } from '@apollo/client'
-import { GET_USERS } from '../queries/queries'
-import { GetUsersQuery } from '../types/generated/graphql'
+import { useMutation, useQuery } from '@apollo/client'
+import { GET_USERS, CREATE_USER } from '../queries/queries'
+import { GetUsersQuery, CreateUserMutation } from '../types/generated/graphql'
 
 const HasuraFetch = () => {
+  const [input, setInput] = useState({ id: '', name: '' })
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
     fetchPolicy: 'cache-and-network',
+  })
+
+  const [insert_users_one] = useMutation<CreateUserMutation>(CREATE_USER, {
+    update(cache, { data: { insert_users_one } }) {
+      const cacheId = cache.identify(insert_users_one)
+      cache.modify({
+        fields: {
+          users(existingUsers, { toReference }) {
+            return [toReference(cacheId), ...existingUsers]
+          },
+        },
+      })
+    },
   })
 
   if (error)
@@ -17,6 +32,34 @@ const HasuraFetch = () => {
 
   return (
     <Layout title="hasura-fetch">
+      <form className="">
+        <input
+          className="border px-2"
+          type="text"
+          value={input.name}
+          onChange={(e) => setInput({ ...input, name: e.target.value })}
+        />
+        <button
+          className="px-2"
+          disabled={!input.name}
+          // TODO 綺麗にする
+          onClick={async (e) => {
+            e.preventDefault()
+            try {
+              await insert_users_one({
+                variables: {
+                  name: input.name,
+                },
+              })
+            } catch (error) {
+              alert(error.message)
+            }
+            setInput({ id: '', name: '' })
+          }}
+        >
+          追加
+        </button>
+      </form>
       {data?.users.map((user) => {
         return <p key={user.id}>{user.name}</p>
       })}
